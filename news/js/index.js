bf209fa6d3d978694704910a520f01e1afdfee9d
fengguorong <1004635391@qq.com>
2016-12-19 08:40:30 +0800

    stress

--- news/js/index.js
+++ news/js/index.js
@@ -1,5 +1,5 @@
 $(function(){
- // 闪屏页   
+// 闪屏页   
     var widths=$(window).width();
     var heights=$(window).height();
     $(".swiper-slidess").css({"height":heights+"px","width":widths+"px"});
@@ -40,10 +40,59 @@ $(function(){
         $(".tubiao").eq(0).css("color","#DF3031");
     }
     
-
+//menu的高度
+    $(".menus").css("height",heights+"px");
     
+// 移动端固定定位的问题
+    var myScroll = new IScroll('#wrapper', {
+        bounceEasing: 'elastic',
+        bounceTime: 1200 ,
+        mouseWheel:true,
+        bounce:true,
+        click:true
+    });
+    myScroll.on("scrollStart",function(){
+        if(myScroll.y==myScroll.maxScrollY){
+            down();
+        }
+        if(myScroll.y==0){
+            up();
+        }
+    })
+    var upEle=$(".up")[0];
+    var downEle=$(".down")[0];
+    var scroller=$(".scroller")[0];
+    function up(){
+        upEle.style.display="block";
+        setTimeout(function(){
+            upEle.style.display="none";;
+        },1000)
+    }
+    function down(){
+        downEle.style.display="block";
+        myScroll.refresh();
+        setTimeout(function(){
+            var news=document.createElement("div");
+            news.style.cssText="font-size:0.3rem;text-align:center;";
+            news.innerHTML="update内容";
+            $("#scroller").append(news);
+            downEle.style.display="none";
+            myScroll.refresh();
+        },2000)
+    }
 
 
+//菜单栏的出现和消失
+    $(".menuss-box").click(function(){
+        $(".menus").css({
+            animation:"menu 1s ease forwards"
+        });
+    })
+    $(".close-nav").click(function(){
+        $(".menus").css({
+            animation:"menu2 1s ease forwards"
+        });
+    })
 
 
 })
